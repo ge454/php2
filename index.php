a3996bae065fef759df80170df594ba7697ef051
bett1n <juliabrbe2@gmail.com>
2024-11-26 08:37:51 -0300

    commit

--- index.php
+++ index.php
@@ -38,11 +38,11 @@
                     }
 
                     function capital(){
-                        return $GLOBALS['n1'] / $GLOBALS['n4'] /100 *$GLOBALS['n3'] ;
+                        return $GLOBALS['n1'] / $GLOBALS['n4'] /100 * $GLOBALS['n3'] ;
                     }
 
                     function prazo(){
-                        return $GLOBALS['n1'] * $GLOBALS['n2'];
+                        return $GLOBALS['n1'] / $GLOBALS['n4'] /100 * $GLOBALS['n2'];
                     }
 
                     function taxa(){
@@ -54,7 +54,7 @@
                     } else if ($_GET['op'] ==  'capital'){
                         echo "<h1> $n1 / $n4/100 * $n3 = " .capital(). "</h1>";
                     } else if ($_GET['op'] ==  'prazo'){
-                        echo "<h1> $n1 * $n2 =" .prazo(). "</h1>";
+                        echo "<h1> $n1 / $n4/100 * $n2  =" .prazo(). "</h1>";
                     } else if ($_GET['op'] ==  'taxa'){
                         echo "<h1> $n1 / $n2 =" .taxa(). "</h1>";
                     }
